13a97c94f6a9b5dada1fc02c3f869230f51e58dc
Tomas De Pietro <tomas.depietro@prosoftwarefactory.com.ar>
2020-08-01 19:51:01 -0300

    read and use config for stream url

--- react/components/webcam/WebcamScreen.js
+++ react/components/webcam/WebcamScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Img} from 'react-image';
 import styled from 'styled-components'
 import image from '../../assets/broken-image.png';
+import Config from 'Config';
 
 const WebcamWrapper = styled.div`
   align-items: center;
@@ -10,11 +11,11 @@ const WebcamWrapper = styled.div`
 `
 
 export const WebcamScreen = () => {
- 
+  
   return (
     <WebcamWrapper>
       <Img
-        src={['http://localhost:5000/video_feed',
+        src={[Config.backendEndpoint + '/video_feed',
               image]}
       />
     </WebcamWrapper>
