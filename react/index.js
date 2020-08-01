f3957f1f0ccddce69c72c4859c25733a7a4c1949
Tomas De Pietro <tomas.depietro@prosoftwarefactory.com.ar>
2020-08-01 17:07:07 -0300

    remove comments

--- react/index.js
+++ react/index.js
@@ -1,13 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-//import HelloWord from './components/App'
-
-/*ReactDOM.render(
-    <HelloWord/>
-	, document.getElementById('root')
-)*/
-
 import { IADetectorApp } from './IADetectorApp';
 
 ReactDOM.render(
