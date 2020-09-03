6b240f068ebe8c2ece0274c1bd88305ad308bdb3
Tomas De Pietro <tomas.depietro@prosoftwarefactory.com.ar>
2020-09-02 23:05:52 -0300

    add socket.io and toast component

--- react/components/webcam/WebcamScreen.js
+++ react/components/webcam/WebcamScreen.js
@@ -3,6 +3,8 @@ import {Img} from 'react-image';
 import styled from 'styled-components'
 import image from '../../assets/broken-image.png';
 import Config from 'Config';
+import io from 'socket.io-client';
+import { useToasts } from 'react-toast-notifications'
 
 const WebcamWrapper = styled.div`
   align-items: center;
@@ -10,12 +12,27 @@ const WebcamWrapper = styled.div`
   justify-content: center;
 `
 
+let endpoint = Config.backendEndpoint;
+let socket = io.connect(endpoint);
+
 export const WebcamScreen = () => {
+
+  const { addToast } = useToasts()
+
+  //const getMessages = () => {
+  socket.on('alarm', data => {
+    console.log(data);
+    addToast('Saved Successfully', { appearance: 'success' })
+  });
+  //};
+
   return (
+    
     <WebcamWrapper>
       <Img
         src={[Config.backendEndpoint + '/video_feed', image]}
       />
     </WebcamWrapper>
+
   );
 };
