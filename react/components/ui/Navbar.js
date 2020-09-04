179f0d80036090beff9911d8e78dbd39f813622a
lucascepeda007 <lucascepeda007@gmail.com>
2020-09-04 10:58:27 -0300

    camera

--- react/components/ui/Navbar.js
+++ react/components/ui/Navbar.js
@@ -1,29 +1,59 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import imagen from './imagen.png';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 200,
+      minHeight:50,
+      marginLeft:100,
+      marginRight:50
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+    root: {
+      width: '100%',
+      height: 400,
+      maxWidth: 300,
+      backgroundColor: theme.palette.background.paper,
+    },
+    center:{
+      justifyContent: "center",
+      alignItems: "center",
+      verticalAlign: "middle",
+      marginRight:50,
+      marginLeft:20,
+    },
+    rtd:{
+        marginRight:10,
+        marginLeft:10,
+      },
+    topcorner:{ 
+      position:"absolute",
+       top:0,
+      right:0, },
+  }));
 
 export const Navbar = () => {
+    const classes = useStyles();
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <img src={imagen} width="40" height="40" alt="Imagen" />
+            <div className={classes.rtd}><img src={imagen} width="40" height="40" alt="Imagen" /></div>
+            
             <Link 
                 className="navbar-brand" 
                 to="/"
             >
-                IA Detector
+                RTD
             </Link>
 
             <div className="navbar-collapse">
                 <div className="navbar-nav">
 
-                <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/camera"
-                    >
-                        Cámara
-                    </NavLink>
+                
                     
                     <NavLink 
                         activeClassName="active"
@@ -63,7 +93,7 @@ export const Navbar = () => {
                         exact
                         to="/login"
                     >
-                        Logout
+                        Salir
                     </NavLink>
                 </ul>
             </div>
