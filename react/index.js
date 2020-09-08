dd09bec1dcf3f072d8c800a96e9ac000e08ebee2
lucascepeda007 <lucascepeda007@gmail.com>
2020-09-07 22:53:55 -0300

    login v2

--- react/index.js
+++ react/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { IADetectorApp } from './IADetectorApp';
 
+import { IADetectorApp } from './IADetectorApp';
+// import './styles/styles.scss'
 
 ReactDOM.render(
   <IADetectorApp />,
--- react/routers/AppRouter.js
+++ react/routers/AppRouter.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Redirect
   } from 'react-router-dom';
 
 import { useDispatch } from 'react-redux';
