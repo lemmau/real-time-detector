02fb791cd4615d41570a18bdc142b64d61db2010
Tomas De Pietro <tomas.depietro@prosoftwarefactory.com.ar>
2020-09-10 00:22:57 -0300

    logic play alarm

--- react/components/webcam/WebcamScreen.js
+++ react/components/webcam/WebcamScreen.js
@@ -5,6 +5,7 @@ import image from '../../assets/broken-image.png';
 import Config from 'Config';
 import io from 'socket.io-client';
 import { useToasts } from 'react-toast-notifications'
+import alarmSound from '../../assets/alarm_sound.mp3'
 
 const WebcamWrapper = styled.div`
   align-items: center;
@@ -18,10 +19,18 @@ const socket = io.connect(endpoint);
 export const WebcamScreen = () => {
 
   const { addToast } = useToasts();
-  
+  let alarm = new Audio(alarmSound);
+
   useEffect( () => {
     socket.off('alarm').on('alarm', data => {
-      addToast('Infraccion Detectada', { appearance: 'error' });
+
+      const shouldSoundTrigger = data['audio']
+      
+      if(shouldSoundTrigger){
+        alarm.play()
+      }
+
+      addToast('Infraccion Detectada', { appearance: 'error', autoDismiss: 'true', autoDismissTimeout: '5000' });
     });
   }, []);
 
