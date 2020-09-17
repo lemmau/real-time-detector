dd1298d970c2ed80e7ad65b7db1706e6bc58085d
Agustin Bellorini <agustin.bellorini@southworks.com>
2020-09-17 19:09:36 -0300

    navBar improvement

--- react/components/ui/Navbar.js
+++ react/components/ui/Navbar.js
@@ -1,12 +1,54 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import imagen from '../../assets/rtd-icon.png';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 200,
+      minHeight:50,
+      marginLeft:100,
+      marginRight:50
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+    root: {
+      width: '100%',
+      height: 400,
+      maxWidth: 300,
+      backgroundColor: theme.palette.background.paper,
+    },
+    center:{
+      justifyContent: "center",
+      alignItems: "center",
+      verticalAlign: "middle",
+      marginRight:50,
+      marginLeft:20,
+    },
+    rtd:{
+        marginRight:10,
+        marginLeft:10,
+      },
+    topcorner:{ 
+      position:"absolute",
+       top:0,
+      right:0, },
+  }));
 
 export const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-      <Link className="navbar-brand" to="/">
-        IA Detector
-      </Link>
+    const classes = useStyles();
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className={classes.rtd}><img src={imagen} width="40" height="40" alt="Imagen" /></div>
+            
+            <Link 
+                className="navbar-brand" 
+                to="/"
+            >
+                RTD
+            </Link>
 
       <div className="navbar-collapse">
         <div className="navbar-nav">
@@ -18,7 +60,6 @@ export const Navbar = () => {
           >
             Configuración
           </NavLink>
-
           <NavLink
             activeClassName="active"
             className="nav-item nav-link"
