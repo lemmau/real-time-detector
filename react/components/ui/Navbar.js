0bb0fa7d06f1829ef4bc2afd4566bdd548bafa42
pablomauu <pablomauu@hotmail.com>
2020-10-17 20:25:42 -0300

    delete navlink 'Salir' - Deprecated

--- react/components/ui/Navbar.js
+++ react/components/ui/Navbar.js
@@ -79,18 +79,6 @@ export const Navbar = () => {
         </div>
       </div>
 
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-        <ul className="navbar-nav ml-auto">
-          <NavLink
-            activeClassName="active"
-            className="nav-item nav-link"
-            exact
-            to="/camera"
-          >
-            Salir
-          </NavLink>
-        </ul>
-      </div>
     </nav>
   );
 };
